0908d79941a85f4ce0c9d399c545192195392503
JulieDuong <duong.julie@webisoft.com>
2020-07-23 23:02:19 -0400

    initial commit + setup

---
+++ src/components/Receipt.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Receipt() {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    font-family: 'Roboto', sans-serif;
+    background-color: #C2C5BB;
+    height: 100vh;
+`
